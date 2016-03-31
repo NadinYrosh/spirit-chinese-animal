1a532f74c0a847ebe5bba735bd607920dc0b0ace
Kellie Olson and Nadiya Yeroshkina <nadiya@>
2016-03-31 16:04:44 -0700

    add js working logic to determine rat or ox animal

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,41 @@
-// var I = 1
-// var V = 5
-// var X = 10
-// var L = 50
-// var C = 100
-// var D = 500
-// var M = 1000
-//
-//
-//
-//   var convertableNumber = function(input) { //this will determine the space
-//     var total =1;
-//     debugger
-//     for (var i=0; i<1; i++) {
-//       total = i;
-//
-//     }
-//     return total;
-//   }
-//
-//
-//
-
-//
+// var rat = ["1912", "1924", "1936", "1948", "1960", "1972", "1984", "1996", "2008"];
+// var ox = ["1913", "1925", "1937", "1949", "1961", "1973", "1985", "1997", "2009"];
+// var tiger = [1914, 1926, 1938, 1950, 1962, ]
+var rat;
+var ox;
+var tiger;
+var rabbit;
+var dragon;
+var snake;
+var horse;
+var sheep;
+var monkey;
+var rooster;
+var dog;
+var pig;
 
 
-function toRomanNumeral(number) {
-  // numberArray = number.toString().split("").reverse();
-  var numberArray = number.toString().split("").reverse();
-  var singleDigits = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-  var tensDigits = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-  var hundredDigits = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-  var thouDigits = ["", "M", "MM", "MMM", "MMMM"];
-
-  var results = '';
-  // debugger
-
-  for (i = 0; i < numberArray.length; i++) {
-    var num = parseInt(numberArray[i]);
-    console.log(num);
-    if (i === 0) {
-      num = singleDigits[num];
-      results = num + results;
-    } else if (i === 1) {
-      num = tensDigits[num];
-      results = num + results;
-    } else if (i === 2) {
-      num = hundredDigits[num];
-      results = num + results;
-    } else if (i === 3){
-      num = thouDigits[num];
-      results = num + results;
-    }
+var calculation = function(year) {
+  var yearCalc = (year - 1912) % 12;
+  console.log(yearCalc);
+  if (yearCalc === 0) {
+    var result = "rat"
+  } else if (yearCalc === 1) {
+    var result = "ox";
   }
-  return results;
+  return result;
 };
+
+
+
+
+
 
 $(document).ready(function() {
   $("form").submit(function(event) { // takes the user input
     event.preventDefault();
-    var input = $("#input").val();
-    var final = toRomanNumeral(input);
+    var formInput = parseInt($("#input").val());
+    var final = calculation(formInput);
     $(".output").text(final);
   });
 });
