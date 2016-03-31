702a3b270282da032b2e8fe82a6f61c9011f42ea
Kellie Olson and Nadiya Yeroshkina <kellie@>
2016-03-31 11:58:54 -0700

    fix the entire logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,65 @@
-var I = 1
-var V = 5
-var X = 10
-var L = 50
-var C = 100
-var D = 500
-var M = 1000
+// var I = 1
+// var V = 5
+// var X = 10
+// var L = 50
+// var C = 100
+// var D = 500
+// var M = 1000
+//
+//
+//
+//   var convertableNumber = function(input) { //this will determine the space
+//     var total =1;
+//     debugger
+//     for (var i=0; i<1; i++) {
+//       total = i;
+//
+//     }
+//     return total;
+//   }
+//
+//
+//
 
-$(function() {
-  var convertableNumber = function(input) { //this will determine the space
+//
 
+
+function toRomanNumeral(number) {
+  // numberArray = number.toString().split("").reverse();
+  var numberArray = number.toString().split("").reverse();
+  var singleDigits = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+  var tensDigits = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+  var hundredDigits = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  var thouDigits = ["", "M", "MM", "MMM", "MMMM"];
+
+  var results = '';
+  // debugger
+
+  for (i = 0; i < numberArray.length; i++) {
+    var num = parseInt(numberArray[i]);
+    console.log(num);
+    if (i === 0) {
+      num = singleDigits[num];
+      results = num + results;
+    } else if (i === 1) {
+      num = tensDigits[num];
+      results = num + results;
+    } else if (i === 2) {
+      num = hundredDigits[num];
+      results = num + results;
+    } else if (i === 3){
+      num = thouDigits[num];
+      results = num + results;
+    }
   }
+  return results;
+};
 
-
-
-  $("form").submit(function(event) { // takes the user input 
+$(document).ready(function() {
+  $("form").submit(function(event) { // takes the user input
     event.preventDefault();
-    var inputNumber = parseInt("#input").val();
-
+    var input = $("#input").val();
+    var final = toRomanNumeral(input);
+    $(".output").text(final);
   });
 });
