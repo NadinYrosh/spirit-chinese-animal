0013b31ad5124001339107961315dff04af7e0c3
Kellie Olson and Nadiya Yeroshkina <kellie@>
2016-03-31 09:12:27 -0700

    add simple logic

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var I = 1
+var V = 5
+var X = 10
+var L = 50
+var C = 100
+var D = 500
+var M = 1000
+
+$(function() {
+  var convertableNumber = function(input) { //this will determine the space
+
+  }
+
+
+
+  $("form").submit(function(event) { // takes the user input 
+    event.preventDefault();
+    var inputNumber = parseInt("#input").val();
+
+  });
+});
